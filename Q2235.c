4d735e47905a45025a9c185215096a83671c6885
GOKUL G <gokulggopi2007@gmail.com>
2026-08-30 21:51:20 +0530

    Add files via upload

---
+++ Q2235.c
@@ -0,0 +1,35 @@
+/*2235. Add Two Integers */
+
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+ 
+
+Example 1:
+Input: num1 = 12, num2 = 5
+Output: 17
+Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+
+Example 2:
+Input: num1 = -10, num2 = 4
+Output: -6
+Explanation: num1 + num2 = -6, so -6 is returned.
+ 
+
+Constraints:
+-100 <= num1, num2 <= 100
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+int sum(int num1, int num2) {
+    return num1+num2;
+}
+int main()
+{
+    int num1,num2;
+    scanf("%d%d",&num1,&num2);
+    int result=sum(num1,num2);
+    printf("%d",result);
+    return 0;
+}
